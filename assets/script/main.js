99a28c92e8f7c290af160596a2aaf3ab9c1bdcb6
draw-de <mdrawde@outlook.com>
2020-02-08 13:07:45 -0500

    zomato testing
    gets city id then goes by cuisines afterwards. going to change this maybe

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,96 +1,95 @@
 $(document).ready(function() {
 
-  var cuisineChoices = ["italian"];
+  // //Spoonacular
+  // var cuisineChoices = ["italian"];
 
-  var apiKey = "e81687065e9f4a2fbbd8244edf86bffd";
+  // var apiKey = "e81687065e9f4a2fbbd8244edf86bffd";
 
-  cuisineChoices.forEach(function(cuisine) {
-    var queryURL = "https://api.spoonacular.com/recipes/search?apiKey="+apiKey+"&cuisine="+cuisine+"&number=5";
-
-    $.ajax({
-      url: queryURL,
-      method: 'GET'
-    }).then(function(object) {
-      var recepies = object.results;
-      recepies.forEach(function(recepie) {
-        var recepieId = recepie.id;
-
-        getRecepieInformation(recepieId);
-      })
-    })
-  })
-
-
-  function getRecepieInformation(id) {
-    $.ajax({
-      url:
-        "https://api.spoonacular.com/recipes/"+id+"/information?apiKey="+apiKey+"",
-      method: "GET"
-    }).then(function(object) {
-      var recepieTitle = object.title;
-      var recepieReadyTime = object.readyInMinutes;
-      var recepieLink = object.sourceUrl;
-
-      $(".recepies").append("<h1>"+recepieTitle+"</h1><h3>Ready in "+recepieReadyTime+" minutes.</h3><a href='"+recepieLink+"' target='_blank'>"+recepieLink+"</a>")
-    });
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // // food recepies api
-
-  // var cuisineChoices = ["Spanish", "Italian", "Greek"];
-
-  // // cuisineChoices.forEach(function(cuisine, index) {
-  //   // var userKey = '95f5bf5fbc0849908c157c0e5f786a3f';
-  //   // var queryURL = "https://api.spoonacular.com/recipes/search?apiKey="+ userKey +"?&query="+cuisine+"";
+  // cuisineChoices.forEach(function(cuisine) {
+  //   var queryURL = "https://api.spoonacular.com/recipes/search?apiKey="+apiKey+"&cuisine="+cuisine+"&number=5";
 
   //   $.ajax({
-  //     url: "https://yummly2.p.rapidapi.com/feeds/search?FAT_KCALMax=1000&maxTotalTimeInSeconds=7200&allowedAttribute=diet-lacto-vegetarian%252Cdiet-low-fodmap&q=chicken%20soup&start=0&maxResult=18",
+  //     url: queryURL,
+  //     method: 'GET'
+  //   }).then(function(object) {
+  //     var recepies = object.results;
+  //     recepies.forEach(function(recepie) {
+  //       var recepieId = recepie.id;
+
+  //       getRecepieInformation(recepieId);
+  //     })
+  //   })
+  // })
+
+
+  // function getRecepieInformation(id) {
+  //   $.ajax({
+  //     url:
+  //       "https://api.spoonacular.com/recipes/"+id+"/information?apiKey="+apiKey+"",
+  //     method: "GET"
+  //   }).then(function(object) {
+  //     var recepieTitle = object.title;
+  //     var recepieReadyTime = object.readyInMinutes;
+  //     var recepieLink = object.sourceUrl;
+
+  //     $(".recepies").append("<h1>"+recepieTitle+"</h1><h3>Ready in "+recepieReadyTime+" minutes.</h3><a href='"+recepieLink+"' target='_blank'>"+recepieLink+"</a>")
+  //   });
+  // }
+
+  //Zomato
+  var zomatoKey = "bc6a01f25d2bf75572f717781b034f8c";
+  var getCity = "miami";
+
+  $.ajax({
+    url: "",
+    method: "GET",
+    headers: {
+      "user-key": zomatoKey
+    }
+  }).then(function(zomato){
+
+  });
+
+
+
+
+
+
+
+
+
+  // $.ajax({
+  //   url: "https://developers.zomato.com/api/v2.1/cities?user-key="+zomatoKey+"&q="+getCity,
+  //   method: "GET",
+  //   headers: {
+  //     "user-key": zomatoKey
+  //   }
+  // }).then(function(city){
+  //   console.log(city);
+  //   var cities = city.location_suggestions;
+
+  //   cities.forEach(function(location) {
+  //     $(".choose-city")
+  //       .append(
+  //         $("<h3>"+location.name+"</h3>")
+  //           .on('click', function() {
+  //             var cityId = location.id;
+  //             cuisineCity(cityId);
+  //           })
+  //       );
+      
+  //   });
+  // });
+  // function cuisineCity(cuisineList){
+  //   $.ajax({
+  //     url: "https://developers.zomato.com/api/v2.1/cuisines?user-key="+zomatoKey+"&city_id="+cuisineList,
   //     method: "GET",
   //     headers: {
-  //       "x-rapidapi-host": "yummly2.p.rapidapi.com",
-  //       "x-rapidapi-key": "f51812f028mshdd8f09e40c06160p1de823jsn3d73c11857ef"
+  //       "user-key": zomatoKey
   //     }
-  //   }).then(function(object) {
-  //     console.log(object);
-  //   })
-  // // })
-
-
-
-
-   //restaurant search api
-  //  var city = 'miami';
-
-  //  var restApi = 'bc6a01f25d2bf75572f717781b034f8c';
-  //  var restURL = 'https://developers.zomato.com/api/v2.1/cuisines?city_id=291';
-
-
-  //  $.ajax({
-  //    url: restURL,
-  //    method: 'GET',
-  //    headers:{
-  //     'user-key': restApi
-  //    }
-  //  }).then(function(object) {
-  //    console.log(object);
-  //  })
+  //   }).then(function(cuisines) {
+  //     console.log(cuisines)
+      
+  //   });
+  // };
 });
