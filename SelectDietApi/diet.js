610556e1f6ba1b3c8313d8b0aa68d8f484af6a57
jonathan manuel camargo labeau <jmcamargo12@gmail.com>
2020-02-10 19:55:54 -0500

    second switcharoo

--- SelectDietApi/diet.js
+++ SelectDietApi/diet.js
@@ -4,67 +4,70 @@ var balDiet = $(".balanced");
 var fatDiet = $(".goodFat");
 var nutInfo = $(".nutValues");
 
-$(".upperleft").on("mouseenter ", function() {
-  carbDiet.animate({ height: "300px" });
+$(".upperleft").on("click ", function() {
+  carbDiet.animate({ height: "400px" });
   proDiet.animate({ height: "0px" });
   balDiet.animate({ height: "0px" });
   fatDiet.animate({ height: "0px" });
-  nutInfo.animate({ opacity: "1" });
-});
 
-$(".upperleft").on("click ", function() {
-  carbDiet.animate({ height: "400px" });
-});
-
-$(".upperright").on("mouseenter ", function() {
-  proDiet.animate({ height: "300px" });
-  carbDiet.animate({ height: "0" });
-  fatDiet.animate({ height: "0px" });
-  balDiet.animate({ height: "0" });
-  $(".title").replaceWith("<h4>Protein Based</h4>");
-  $(".facts").replaceWith(
-    "<p>Proteins are essential nutrients for the human body. They are one of the building blocks of body tissue and can also serve as a fuel source.</p>"
+  $(".title").text("The Carb Way");
+  $(".facts").text(
+    "Proteins are essential nutrients for the human body. They are one of the building blocks of body tissue and can also serve as a fuel source."
   );
-  $(".dietbenefits").replaceWith("<h4>Build muscle mass and tissue</h4>");
-  $(".typeone").replaceWith("<p>High Protein</p>");
-  $(".typetwo").replaceWith("<p>Low Protein</p>");
+  $(".benefits").text("Health Impact");
+  $(".dietbenefits").text("Energy  & Stamina ");
+  $(".ouroptions").text("Our Recepies Selections");
+  $(".typeone").text("High Carbs");
+  $(".typetwo").text("Low Carbs");
 });
 
 $(".upperright").on("click ", function() {
   proDiet.animate({ height: "400px" });
-});
+  carbDiet.animate({ height: "0px" });
 
-$(".leftdown").on("mouseenter ", function() {
-  proDiet.animate({ height: "0" });
-  carbDiet.animate({ height: "0" });
+  balDiet.animate({ height: "0px" });
   fatDiet.animate({ height: "0px" });
-  balDiet.animate({ height: "300px" });
-  $(".title").replaceWith("<h4>Balanced Route</h4>");
-  $(".facts").replaceWith(
-    "<p>A healthy diet is a diet that helps to maintain or improve overall health. Provides the body with essential nutrition: fluid, macronutrients, micronutrients, and adequate calories.</p>"
+
+  $(".title").text("Protein Based");
+  $(".facts").text(
+    "Proteins are essential nutrients for the human body. They are one of the building blocks of body tissue and can also serve as a fuel source."
   );
-  $(".dietbenefits").replaceWith("<h4>Improves Health</h4>");
-  $(".typeone").replaceWith("<p>Balanced Alternatives</p>");
+  $(".benefits").text("Health Impact");
+  $(".dietbenefits").text("Build muscle mass and tissue");
+  $(".ouroptions").text("Our Recepies Selections");
+  $(".typeone").text("High Protein");
+  $(".typetwo").text("Low Protein");
 });
 
 $(".leftdown").on("click ", function() {
   balDiet.animate({ height: "400px" });
-});
+  carbDiet.animate({ height: "0px" });
+  proDiet.animate({ height: "0px" });
+  fatDiet.animate({ height: "0px" });
 
-$(".rightdown").on("mouseenter ", function() {
-  proDiet.animate({ height: "0" });
-  carbDiet.animate({ height: "0" });
-  balDiet.animate({ height: "0px" });
-  fatDiet.animate({ height: "300px" });
-  $(".title").replaceWith("<h4>Fat Diet</h4>");
-  $(".facts").replaceWith(
-    "<p>Fats are one of the three main macronutrients, along with carbohydrates and proteins.</p>"
+  $(".title").text("Balanced Route");
+  $(".facts").text(
+    "A healthy diet is a diet that helps to maintain or improve overall health. Provides the body with essential nutrition: fluid, macronutrients, micronutrients, and adequate calories."
   );
-  $(".dietbenefits").replaceWith("<h4>Gain or lose weight</h4>");
-  $(".typeone").replaceWith("<p>High Fat</p>");
-  $(".typetwo").replaceWith("<p>Low Fat</p>");
+  $(".benefits").text("Health Impact");
+  $(".dietbenefits").text("Improve Health");
+  $(".ouroptions").text("Our Recepies Selections");
+  $(".typeone").text("Balanced Alternative");
 });
 
 $(".rightdown").on("click ", function() {
   fatDiet.animate({ height: "400px" });
+  balDiet.animate({ height: "0px" });
+  carbDiet.animate({ height: "0px" });
+  proDiet.animate({ height: "0px" });
+
+  $(".title").text("Fat Diet");
+  $(".facts").text(
+    "Fats are one of the three main macronutrients, along with carbohydrates and proteins."
+  );
+  $(".benefits").text("Health Impact");
+  $(".dietbenefits").text("Gain or lose weight");
+  $(".ouroptions").text("Our Recepies Selections");
+  $(".typeone").text("High Fat");
+  $(".typetwo").text("Low Fat");
 });
