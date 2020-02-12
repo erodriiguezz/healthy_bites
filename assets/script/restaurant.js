adb2932bf1b27bab01ef2a9059b388f8de643115
draw-de <mdrawde@outlook.com>
2020-02-12 17:22:28 -0500

    Search feature & missing css

--- assets/script/restaurant.js
+++ assets/script/restaurant.js
@@ -33,49 +33,56 @@ $(document).ready(function() {
       });
     });
     function City(city) {
-      var qZomato = "healthy";
       // 1 = delivery / 5 = Takeaway / 6 = Cafes / 7 = Daily menus / 8 = breakfast / 9 = lunch / 10 = dinner / 13 = Pocket Friendly Delivery
       var categoryZomato = ["1", "5", "6", "7", "8", "9", "10", "11", "13"];
       var sortZomato = ["rating", "cost", "real_distance"];
 
-      $.ajax({
-        url:
-          "https://developers.zomato.com/api/v2.1/search?user-key=" +
-          zomatoKey +
-          "&entity_id=" +
-          city +
-          "&entity_type=city&q=" +
-          qZomato +
-          "&count=10&category=" +
-          categoryZomato[8] +
-          "&sort=" +
-          sortZomato[0],
-        method: "GET",
-        headers: {
-          "user-key": zomatoKey
-        }
-      }).then(function(zomato) {
-        console.log(zomato);
-        var rName = zomato.restaurants;
-        rName.forEach(function(obj) {
-          var name = obj.restaurant.name;
-          var time = obj.restaurant.timings;
-          var cuisines = obj.restaurant.cuisines;
-          var rating = obj.restaurant.user_rating.aggregate_rating;
-          var address = obj.restaurant.location.address;
-          $(".zomato").append(
-            "<div class='restaurant-list'><h4 class='rName'>" +
-              name +
-              "</h4><ul><li class='rList'>Rating: " +
-              rating +
-              "</li><li class='rList'>Business Hours: " +
-              time +
-              "</li><li class='rList'>Cuisines: " +
-              cuisines +
-              "</li><li class='rList'>Address: " +
-              address +
-              "</li></div>"
-          );
+      $(".zomato").html(
+        "<div>Click on food type<div><input id='querySearch' type='text' placeholder='Vegen' /> <input type='submit' id='querySubmit' /></div></div><div class='restaurants'></div>"
+      );
+      $("#querySubmit").on("click", function() {
+        var qZomato = $("#querySearch").val();
+        $(".restaurants").empty();
+
+        $.ajax({
+          url:
+            "https://developers.zomato.com/api/v2.1/search?user-key=" +
+            zomatoKey +
+            "&entity_id=" +
+            city +
+            "&entity_type=city&q=" +
+            qZomato +
+            "&count=10&category=" +
+            categoryZomato[8] +
+            "&sort=" +
+            sortZomato[0],
+          method: "GET",
+          headers: {
+            "user-key": zomatoKey
+          }
+        }).then(function(zomato) {
+          console.log(zomato);
+          var rName = zomato.restaurants;
+          rName.forEach(function(obj) {
+            var name = obj.restaurant.name;
+            var time = obj.restaurant.timings;
+            var cuisines = obj.restaurant.cuisines;
+            var rating = obj.restaurant.user_rating.aggregate_rating;
+            var address = obj.restaurant.location.address;
+            $(".restaurants").append(
+              "<div class='restaurant-list'><h4 class='rName'>" +
+                name +
+                "</h4><ul><li class='rList'>Rating: " +
+                rating +
+                "</li><li class='rList'>Business Hours: " +
+                time +
+                "</li><li class='rList'>Cuisines: " +
+                cuisines +
+                "</li><li class='rList'>Address: " +
+                address +
+                "</li></div>"
+            );
+          });
         });
       });
     }
